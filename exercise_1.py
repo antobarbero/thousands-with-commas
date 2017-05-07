6e913cc2d38d52604f331fb829fe6e675bd6b096
antobarbero <antobarbero94@gmail.com>
2017-05-07 20:56:28 -0300

    cambio copia de lista

--- exercise_1.py
+++ exercise_1.py
@@ -1,11 +1,10 @@
-from copy import copy
 
 def thousands_with_commas(i):
 	"""Receive an integer and returns a string with the number
 	with commas each three digits"""
 
 	list_i = list(str(i))
-	tmp_list_i = copy(list_i)
+	tmp_list_i = list_i[:]
 	for n in range(len(list_i)-3, 0, -3):
 		tmp_list_i.insert(n, ',')
 	
