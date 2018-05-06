2a08ae2884539065aa595546a14ab4eff009adc1
Anto <antobarbero94@gmail.com>
2018-05-06 19:22:31 -0300

    Update exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -1,16 +1,18 @@
 
 def thousands_with_commas(i):
-	"""Receive an integer and returns a string with the number
-	with commas each three digits"""
+    """
+    Receive an integer and returns a string with the number
+    with commas each three digits
+    """
 
-	if not type(i) == int:
-		raise TypeError("Parameter must be an integer.")
+    if not type(i) == int:
+        raise TypeError("Parameter must be an integer.")
 	
-	i = "{:,}".format(i)		
-	return str(i)
+        i = "{:,}".format(i)		
+        return str(i)
 	
 
 if __name__ == '__main__':
-	assert thousands_with_commas(1234) == '1,234'
-	assert thousands_with_commas(123456789) == '123,456,789'
-	assert thousands_with_commas(12) == '12'
+    assert thousands_with_commas(1234) == '1,234'
+    assert thousands_with_commas(123456789) == '123,456,789'
+    assert thousands_with_commas(12) == '12'
